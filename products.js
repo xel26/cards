f4fcbb1ff97a6b2fc9b99e10cad36b4f90e55213
xel26 <zhxo111nvshq@gmail.com>
2023-10-20 17:53:29 +0700

    update there is only one card

--- products.js
+++ products.js
@@ -9,28 +9,6 @@ const Products = [
             "https://s.lazada.co.id/s.NRkKE?cc",
             "https://s.lazada.co.id/s.NRRR7?cc"
         ]
-    },
-    {
-        img: "products/card1.png",
-        category: "sage",
-        links: [
-            "https://s.lazada.co.id/s.NRpwS?cc",
-            "https://s.lazada.co.id/s.NRpVK?cc",
-            "https://s.lazada.co.id/s.NRok4?cc",
-            "https://s.lazada.co.id/s.NRkKE?cc",
-            "https://s.lazada.co.id/s.NRRR7?cc"
-        ]
-    },
-    {
-        img: "products/card1.png",
-        category: "sage",
-        links: [
-            "https://s.lazada.co.id/s.NRpwS?cc",
-            "https://s.lazada.co.id/s.NRpVK?cc",
-            "https://s.lazada.co.id/s.NRok4?cc",
-            "https://s.lazada.co.id/s.NRkKE?cc",
-            "https://s.lazada.co.id/s.NRRR7?cc"
-        ]
     }
 ]
 
